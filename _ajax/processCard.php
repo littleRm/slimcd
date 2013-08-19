ca453135a997f301372924e9581eeec1220cc2f2
W. Ripley <wripley@dwgreen.com>
2013-08-19 14:54:00 -0700

    updated credentials info

--- _ajax/processCard.php
+++ _ajax/processCard.php
@@ -3,17 +3,30 @@
 	 
 	 include_once "../_inc/slimcd.php";
 	 
+	 $privateToken = "{ENTER YOUR PRIVATE TOKEN USERNAME HERE}";
+	 // test credentials
+	 $clientid = "1032";
+	 $siteid = "228226448";
+	 $priceid = "74";
+
+	 // live credentials
+	 /*
+	 $clientid = "";
+	 $siteid = "";
+	 $priceid = "";
+	 */
+	 
 	//process credit card transaction using slimCD API
 	if(!empty($_SESSION['info']['gateID']) || !empty($_SESSION['info']['cvvToken'])){
 		$SlimCD = new SlimCD();
 		
 		// Create a ProcessTransaction Request class
 		$request = new Transact_ProcessTransactionRequest();
-		$request->username = 'R4Q4QPY4';
+		$request->username = $privateToken;
 		$request->transtype = 'SALE';
-		$request->clientid = '1032';
-		$request->siteid = 228226448;
-		$request->priceid = 74 ;
+		$request->clientid = $clientid;
+		$request->siteid = $siteid;
+		$request->priceid = $priceid;
 		$request->amount = 19.77;
 		if(!empty($_SESSION['info']['gateID']))
 			$request->gateid = $_SESSION['info']['gateID'];
